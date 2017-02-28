9c59bd7cd7c7ed7179689691a546a132520ceb1e
Kristina Beck <kristina.beck@HOTB.local>
2017-02-28 07:29:50 -0800

    fixed alignment of confirmation inputs, added client-side form validation errors

--- src/web/content/js/custom.js
+++ src/web/content/js/custom.js
@@ -7,4 +7,27 @@ $(document).ready(function(){
         $('#addContact').addClass('hidden');
         $('#phoneNotifications').removeClass('hidden');
     });
-})
+
+    var email = $('input.email').val();
+
+    if (email !== '') {  // If something was entered
+        if (!isValidEmailAddress(email)) {
+            $('label.email-error'.show(); //error message
+            $('input.email').focus();   //focus on email field
+            return false;
+        }
+    }
+
+    function isValidEmailAddress(emailAddress) {
+        var pattern = new RegExp(/^(('[\w-+\s]+')|([\w-+]+(?:\.[\w-+]+)*)|('[\w-+\s]+')([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
+        return pattern.test(emailAddress);
+    };
+
+});
+
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
