74dd5421ea90eb8659a3feed7bc893dcdeb05fb2
kwitts <kristiwitts@gmail.com>
2017-03-01 14:34:54 -0800

    delete button to added locations

--- src/web/content/js/addlocation.js
+++ src/web/content/js/addlocation.js
@@ -20,12 +20,17 @@ $(document).ready(function(){
                     //window.location.href = "/user/index.html";
                     for(i=0;i<result.locations.length;i++)
                     {
-                        var appendedval = "<div class='user-block col-sm-3 col-xs-6'><span class='glyphicon glyphicon-map-marker'></span><span class='added-location'>" + result.locations[i].displayName + "</span> <a href='#' class='btn btn-primary btn-block' onclick='EditLocation("+result.locations[i].id+")'>Edit</a></div>";
+                        var appendedval = "<div class='user-block col-sm-3 col-xs-6'><span class='glyphicon glyphicon-map-marker'></span><span class='added-location'>" + result.locations[i].displayName + "</span> <a href='#' class='btn btn-primary  btn-block-half' onclick='EditLocation(" + result.locations[i].id + ")'>Edit</a> <a href='#' class='btn btn-danger btn-block-half' id='deletelocation(" + result.locations[i].id + ")'>Delete</a> </div>";
+
+
+
                         //console.log(appendedval);
                         if(document.getElementById("locationsrow") != null)
                         {
                             document.getElementById("locationsrow").innerHTML += appendedval;
                         }
+
+
                     }
                 }
             }
@@ -140,7 +145,15 @@ $(document).ready(function(){
             alert("Please enter city or zip.");
             return;
         }
+<<<<<<< 5f7b188ac2662fa6c0cf8a65a366caf071185db7
     });
+=======
+
+
+    });
+
+
+>>>>>>> delete button to added locations
 })
 
 
--- src/web/content/js/custom.js
+++ src/web/content/js/custom.js
@@ -20,18 +20,21 @@ $(document).ready(function(){
 
   });
 
-  $('#deletelocation').click(function() {
-      $('#modal').on('show.bs.modal', function (event) {
-        var modal = $(this);
-        modal.find('.modal-title').text('Confirm Delete Location');
-        modal.find('.modal-body').text('Are you sure you want to delete this location?');
-        modal.find('.modal-footer').html('<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button> <button type="button" class="btn btn-danger" id="confirmdeleteloc">Delete</button>');
-    });
+
+
+/*    $("#deletelocation(" + result.locations[i].id + ")").click(function() {
+        $("#modal").on('show.bs.modal', function (event) {
+          var modal = $(this);
+          modal.find(".modal-title").text("Confirm Delete Location");
+          modal.find(".modal-body").text("Are you sure you want to delete this location?");
+          modal.find(".modal-footer").html("<button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button> <button type='button' class='btn btn-danger' id='confirmdelete(" + result.locations[i].id + ")'>Delete</button>");
+      });
+  });*/
 
 });
 
   $('#btnlogout').click(function() {
-      console.log("in logout button");
+      //console.log("in logout button");
       sessionStorage.clear();
 
 });
