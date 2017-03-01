47fe1d79c6adae280d6c8f394624657e4b4ecc0f
kwitts <kristiwitts@gmail.com>
2017-03-01 10:03:35 -0800

    body content changes, removed phone registration, added settings

--- src/web/content/js/custom.js
+++ src/web/content/js/custom.js
@@ -10,6 +10,25 @@ $(document).ready(function(){
 
     $('.phone').mask('000-000-0000');
 
+    $('#unsubscribe').click(function() {
+        $('#modal').on('show.bs.modal', function (event) {
+          var modal = $(this);
+          modal.find('.modal-title').text('Confirm Unsubscribe');
+          modal.find('.modal-body').text('Are you sure you want to unsubscribe?');
+          modal.find('.modal-footer').html('<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button> <button type="button" class="btn btn-danger" id="confirmunsubscribe">Unsubscribe</button>');
+      });
+
+  });
+
+  $('#deletelocation').click(function() {
+      $('#modal').on('show.bs.modal', function (event) {
+        var modal = $(this);
+        modal.find('.modal-title').text('Confirm Delete Location');
+        modal.find('.modal-body').text('Are you sure you want to delete this location?');
+        modal.find('.modal-footer').html('<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button> <button type="button" class="btn btn-danger" id="confirmdeleteloc">Delete</button>');
+    });
+
+});
 });
 
 function isNumberKey(evt){
